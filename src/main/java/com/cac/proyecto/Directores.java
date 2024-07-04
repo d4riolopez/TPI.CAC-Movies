f6f9374762fdff72dbfe9ea0901beb60fb118922
d4riolopez <26413@sistemas.frc.utn.edu.ar>
2024-07-04 15:55:22 -0300

    actualizando dependencias maven

--- src/main/java/com/cac/proyecto/Directores.java
+++ src/main/java/com/cac/proyecto/Directores.java
@@ -5,5 +5,15 @@ public class Directores {
         private String apellido;
         private int edad; 
         private String nacionalidad;
+        
+        Directores(){};//Constructor sin parametros
+        Directores(String n,String a, int e,String nac)//Constructor con parametros
+        {
+          this.nombre=n;
+          this.apellido=a;
+          this.edad=e;
+          this.nacionalidad=nac;                
+        }
 
+        
 }
