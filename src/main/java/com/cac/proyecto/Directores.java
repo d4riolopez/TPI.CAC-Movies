d32b13002ea0e5d195124c7914a2e75d54e25b7f
d4riolopez <26413@sistemas.frc.utn.edu.ar>
2024-07-04 13:33:47 -0300

    agregado de atributos en clase Peliculas y Directores

---
+++ src/main/java/com/cac/proyecto/Directores.java
@@ -0,0 +1,10 @@
+package com.cac.proyecto;
+
+public class Directores {
+        private int idDirector;
+        private String nombre;
+        private String apellido;
+        private Date f_nac;
+        private String nacionalidad;
+
+}
--- src/main/java/com/cac/proyecto/Peliculas.java
+++ src/main/java/com/cac/proyecto/Peliculas.java
@@ -2,4 +2,12 @@ package com.cac.proyecto;
 
 public class Peliculas {
 
+private int idPelicula;
+    private String nombre;
+    private String foto;
+    private String sinopsis;
+    private String genero;
+    private String clasificacion;
+    private Date fechaEstreno;
+    private int idDirector;
 }
