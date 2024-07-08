3eba9f0637ce978e1587c1545153940a321349eb
d4riolopez <26413@sistemas.frc.utn.edu.ar>
2024-07-08 18:00:57 -0300

    completadala clase sesiones y usuarios

--- src/main/java/com/cac/proyecto/Usuarios.java
+++ src/main/java/com/cac/proyecto/Usuarios.java
@@ -4,11 +4,31 @@ import java.util.Date;
 
 public class Usuarios {
     private String nombre;
-    private String nacionalidad;
     private String apellido;
-    private String contraseña;
     private String email;
+    private String contraseña;
     private Date fechaNacimiento;
-    private int sesiones_id;
-
+    private String nacionalidad;
+    
+    Usuarios (){};
+    Usuarios (String nom, String ape, String em,String cont,Date fNacim,String nac){
+        this.nombre=nom;
+        this.apellido=ape;
+        this.email=em;
+        this.contraseña=cont;
+        this.fechaNacimiento=fNacim;
+        this.nacionalidad=nac;
+    }
+    public String getNombre(){return nombre;};
+    public void setNombre(String nom){this.nombre=nom;};
+    public String getApellido(){return apellido;};
+    public void setApellido(String ape){this.apellido=ape;};
+    public String getEmail(){return email;};
+    public void setEmail(String em){this.email=em;};
+    public String getContraseña(){return contraseña;};
+    public void setContraseña(String cont){this.contraseña=cont;};
+    public Date getFechaNacimiento(){return fechaNacimiento;};
+    public void setFechaNacimiento(Date fNacim){this.fechaNacimiento=fNacim;};
+    public String getNacionalidad(){return nacionalidad;};
+    public void setNacionalidad(String nac){this.nacionalidad=nac;};   
 }
