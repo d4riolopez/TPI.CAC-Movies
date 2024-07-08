7bdd6a705a8e0c3e196115471e92c9764d81b8be
MartinaAsad <lasupermartu15@gmail.com>
2024-07-08 19:35:10 -0300

    agregue script de la base de datos

---
+++ finalcac.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-07-2024 a las 02:55:59
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `finalcac`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `directores`
+--
+
+CREATE TABLE `directores` (
+  `idDirectores` int(11) NOT NULL,
+  `nombre` varchar(35) DEFAULT NULL,
+  `apellido` varchar(35) DEFAULT NULL,
+  `f_nac` date DEFAULT NULL,
+  `nacionalidad` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `idPelicula` int(11) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `foto` varchar(250) DEFAULT NULL,
+  `sinopsis` varchar(500) DEFAULT NULL,
+  `genero` varchar(20) DEFAULT NULL,
+  `clasificacion` varchar(10) DEFAULT NULL,
+  `fechaEstreno` date DEFAULT NULL,
+  `director` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `directores`
+--
+ALTER TABLE `directores`
+  ADD PRIMARY KEY (`idDirectores`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`idPelicula`,`director`),
+  ADD KEY `fk_Peliculas_Directores_idx` (`director`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
